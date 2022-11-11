88ebd2a5fffe8300c1354888326dc62aca7dd2a1
Matheus Julidori <m.henrique@ges.inatel.br>
2022-11-10 22:51:20 -0300

    Lista

--- ADS_Padrao_MVC/src/main/java/br/inatel/labs/labads/padraomvc/model/service/ProdutoService.java
+++ ADS_Padrao_MVC/src/main/java/br/inatel/labs/labads/padraomvc/model/service/ProdutoService.java
@@ -16,9 +16,9 @@ public class ProdutoService {
 	
 	@PostConstruct
 	private void setup() {
-		Produto p1 = new Produto(1L, "Furadeira", 300.00);
-		Produto p2 = new Produto(2L, "Lixadeira", 200.00);
-		Produto p3 = new Produto(3L, "Serra circular", 500.00);
+		Produto p1 = new Produto(1L, "Fonte 750W", 570.00);
+		Produto p2 = new Produto(2L, "Gabinete", 350.00);
+		Produto p3 = new Produto(3L, "RTX 2060 6Gb", 1500.00);
 		
 		listaDeProdutos.add(p1);
 		listaDeProdutos.add(p2);
